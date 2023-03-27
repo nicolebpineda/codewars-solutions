d4c6fea2f76eda615a290087ff9be55058b8743a
Nicole B Pineda <nicolebpineda@gmail.com>
2023-03-26 21:22:04 -0600

    added new codewars challenge

---
+++ 8kyu/convert_a_boolean_to_a_string.js
@@ -0,0 +1,9 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// my solution
+function booleanToString(b){
+    return b === true ? 'true' : 'false'
+   }
+   
