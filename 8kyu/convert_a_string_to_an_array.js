9c339a465921ab818e4efbac57990b2b2688c7b8
Nicole B Pineda <nicolebpineda@gmail.com>
2023-07-04 20:12:25 -1000

    added new codewars challenge

---
+++ 8kyu/convert_a_string_to_an_array.js
@@ -0,0 +1,15 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// My solution
+// Take in a string
+function stringToArray(string){
+    // Return string with each word as element in array
+    return string.split(' ')
+}
+  
+// 'Hello World' => ['Hello', 'World']
+  
