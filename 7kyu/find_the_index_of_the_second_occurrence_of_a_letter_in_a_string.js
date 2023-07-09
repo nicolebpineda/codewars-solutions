80015c92f8381dc52a447427477855ce20f637b1
Nicole B Pineda <nicolebpineda@gmail.com>
2023-07-08 22:32:31 -0600

    added new codewars challenge

---
+++ 7kyu/find_the_index_of_the_second_occurrence_of_a_letter_in_a_string.js
@@ -0,0 +1,17 @@
+// In this kata, you need to write a function that takes a string and a letter as input and then returns the index of the second occurrence of that letter in the string. If there is no such letter in the string, then the function should return -1. If the letter occurs only once in the string, then -1 should also be returned.
+
+// Examples:
+
+// secondSymbol('Hello world!!!','l')  --> 3
+// secondSymbol('Hello world!!!', 'A') --> -1
+
+// My solution
+// Take in a string, and a letter/symbol
+function secondSymbol(s, symbol) {
+    // Return index of second symbol, starting at the index after the first symbol
+    return s.indexOf(symbol, (s.indexOf(symbol) + 1))
+}
+  
+// 'Hi mom', 'm' => 5
+// 'codewars', 'p' => -1
+  
